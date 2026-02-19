cfb978cfb9837aa6f192df0ebbe36fe4c2229c87
Win-Ed54 <edwinfernsv@gmail.com>
2026-02-19 10:37:46 -0600

    fix: coincidir tipos de datos con ordershub

--- kdspro/src/kdspro.Api/Hubs/OrdersHub.cs
+++ kdspro/src/kdspro.Api/Hubs/OrdersHub.cs
@@ -13,10 +13,10 @@ public class OrdersHub : Hub
     public async Task SendOrderToKitchen(object order)
     {
         //Envia el objeto order solo a los miembros del grupo cocina
-        await Clients.Group("cocina").SendAsync("ReceiveNewOrder", order);
+        await Clients.Group("cocina").SendAsync("ReceiveOrder", order);
     }
 
-    public async Task OrderReady(int orderId)
+    public async Task OrderReady(string orderId)
     {
         //Notifica a todos (o a un grupo "meseros") que el pedido X esta listo
         await Clients.All.SendAsync("UpdateOrderStatus", orderId, "Listo");
