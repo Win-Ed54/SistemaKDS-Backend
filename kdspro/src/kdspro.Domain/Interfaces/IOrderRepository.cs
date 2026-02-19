d5d3300ff451ae5b7efd0e204644ee9dee7e2e56
Rene <wakandaforever.4170@gmail.com>
2026-02-19 10:22:19 -0600

    feat: Añadir un metodo nuevo para IOrderRepository

--- kdspro/src/kdspro.Domain/Interfaces/IOrderRepository.cs
+++ kdspro/src/kdspro.Domain/Interfaces/IOrderRepository.cs
@@ -1,11 +1,16 @@
 using kdspro.Domain.Entities;
 using kdspro.Domain.Enums;
 using System.Threading;
-namespace kdspro.Domain.Interfaces;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
-// Al heredar de IGenericRepository<Order>, ya tienes GetAllAsync, GetByIdAsync y CreateAsync
-public interface IOrderRepository : IGenericRepository<Order>
+namespace kdspro.Domain.Interfaces
 {
-    // AQUÍ SOLO VA LO QUE NO ES CRUD GENÉRICO
-    Task UpdateStatusAsync(string id, OrderStatus status, CancellationToken ct = default);
+    // Al heredar de IGenericRepository<Order>, ya tienes GetAllAsync, GetByIdAsync y CreateAsync
+    public interface IOrderRepository : IGenericRepository<Order>
+    {
+        // AQUÍ SOLO VA LO QUE NO ES CRUD GENÉRICO
+        Task UpdateStatusAsync(string id, OrderStatus status, CancellationToken ct = default);
+        Task<List<Order>> GetActiveOrdersAsync(CancellationToken ct = default); // Método para obtener solo las órdenes activas (Pending, InProgress)
+    }
 }
