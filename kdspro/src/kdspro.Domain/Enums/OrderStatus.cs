dc3810b5d30c6ca208ffe676f387f3d30a9e1d7a
Rene <wakandaforever.4170@gmail.com>
2026-02-19 10:56:38 -0600

    feat: Agregar la opcion cancelar

--- kdspro/src/kdspro.Domain/Enums/OrderStatus.cs
+++ kdspro/src/kdspro.Domain/Enums/OrderStatus.cs
@@ -5,5 +5,6 @@ public enum OrderStatus
     Pending,    // Recién llegado
     Preparing,  // El cocinero lo está haciendo
     Ready,      // Ya salió de la cocina
-    Delivered   // Ya se le entregó al cliente
+    Delivered,   // Ya se le entregó al cliente
+    Cancelled   // Pedido cancelado
 }
