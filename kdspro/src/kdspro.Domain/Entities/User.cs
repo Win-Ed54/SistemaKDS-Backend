b008880fbb48e741c1f95c14957c2cb76b6fe119
Edwin <edwinfernsv@gmail.com>
2026-03-10 15:04:15 -0600

    feat: Crear user para mongodb

---
+++ kdspro/src/kdspro.Domain/Entities/User.cs
@@ -0,0 +1,17 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace kdspro.Domain.Entities;
+
+public class User
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+
+    public string Id {get; set;} = "";
+    public string Username {get; set;} = "";
+
+    public string PasswordHash {get; set;} = "";
+
+    public string Role {get; set;} = "";
+}
